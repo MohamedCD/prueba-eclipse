713744bf754c60d9771f31bf7400f63889672ae7
Mohamed <mohamed.chahdi.dkikar@gmail.com>
2021-03-07 12:25:54 +0100

    version 1

---
+++ Prueba control de versiones/src/ejemploGit/ejemplo.java
@@ -0,0 +1,12 @@
+package ejemploGit;
+
+public class ejemplo {
+	
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		
+		System.out.print(a+b);
+		System.out.print(a-b);
+	}
+}
---
+++ Prueba control de versiones/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Mohamed
+ *
+ */
+module Ejemplo {
+}
